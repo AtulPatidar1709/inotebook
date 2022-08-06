f613da2c84218d3a45116a5201004ae713beb8cc
AtulPatidar <atulpatidar1709@gmail.com>
2022-08-07 00:22:09 +0530

    Data Base Connected vid43

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
-const mongoURI = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
+const mongoURI = "mongodb://localhost:27017/"
 
-const connectToMongo = ()=>{
-    mongoose.connect(mongoURI, ()=>{
-        console.log("Connected to Mongo Successfully");
+const connectToMongo = () => {
+    mongoose.connect(mongoURI, () => {
+        console.log("Connted to Mongoes Sucessfully");
     })
 }
 
